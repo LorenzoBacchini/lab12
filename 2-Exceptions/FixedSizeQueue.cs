34ac6f7c86ce0e650734fb40195ff8b8eb5ce7c7
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2022-12-13 13:29:55 +0100

    Es 2 done

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -6,27 +6,34 @@ namespace Exceptions
         private object[] _items;
         private int _firstIndex = 0;
         private int _lastIndex = 0;
-        
+
         public FixedSizeQueue(uint capacity)
         {
             Capacity = capacity;
             _items = new object[capacity];
         }
-        
+
         public uint Capacity { get; }
 
         public uint Count => (uint) (_lastIndex - _firstIndex);
 
         public object GetFirst()
         {
-            // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (_firstIndex == _lastIndex)
+            {
+                throw new EmptyQueueException("La coda e vuota");
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
         }
-        
+
         public void AddLast(object item)
         {
+            if ((_lastIndex-_firstIndex) == Capacity)
+            {
+                throw new FullQueueException("La coda è piena");
+            }
             // TODO ensure objects can only be inserted if the item is queue is not full
             _items[_lastIndex % Capacity] = item;
             _lastIndex++;
